b379584aae3eb32e253a501b1dd378762b3dddd0
gffuma <fumagalli.gf@gmail.com>
2016-07-20 14:52:34 +0200

    Improve nav bar with user data and logout

--- resources/views/layouts/material.blade.php
+++ resources/views/layouts/material.blade.php
@@ -26,12 +26,30 @@
 </head>
 <body>
 
-<nav class="navbar navbar-inverse navbar-fixed-top">
-  <div class="container">
+<div class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="/">Tripopupper</a>
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-inverse-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="{{ url('/') }}">Tripopupper</a>
     </div>
-</nav>
+    <div class="navbar-collapse collapse navbar-inverse-collapse">
+      <ul class="nav navbar-nav navbar-right">
+        <li class="dropdown">
+          <a href="bootstrap-elements.html" data-target="#" class="dropdown-toggle" data-toggle="dropdown">
+            {{ Auth::user()->email }}
+            <b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li><a href="{{ url('/logout') }}">Logout</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
 
 <div class="container-fluid">
   @yield('content')
