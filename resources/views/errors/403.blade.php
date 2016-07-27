b31850c9e1c6bd92f6efb0dc210b13a79e445510
gffuma <fumagalli.gf@gmail.com>
2016-07-27 11:24:08 +0200

    Errors pages

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.material_errors')
+
+@section('content')
+  <div class="text-center">
+    <h1 style="font-size: 72px">403</h1>
+    <h2>Nothing to see here</h2>
+  </div>
+@endsection
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.material_errors')
+
+@section('content')
+  <div class="text-center">
+    <h1 style="font-size: 72px">404</h1>
+    <h2>Not Found</h2>
+  </div>
+@endsection
